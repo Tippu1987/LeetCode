623204daf408033b54a872f6a6d58ceec54752a5
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-01-30 23:30:30 -0700

    Two Sum Problem

---
+++ TwoSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] { 2,7,7,11 };
+            int trg = 13;
+            int[] res = TwoSum(num, trg);
+            Console.WriteLine($"Locations={res[0]},{res[1]}");
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> tempMap = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (tempMap.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { tempMap[target - nums[i]], i };
+                }
+                else
+                {
+                    if (!tempMap.ContainsKey(nums[i]))
+                        tempMap.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
