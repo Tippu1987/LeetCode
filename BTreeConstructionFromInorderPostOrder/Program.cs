873522c71d933a7d688ed8733ba2a4ff8ffa8c88
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-07-26 16:44:11 -0700

    Better Working Soln from LeetCode

--- BTreeConstructionFromInorderPostOrder/Program.cs
+++ BTreeConstructionFromInorderPostOrder/Program.cs
@@ -9,8 +9,8 @@ namespace BTreeConstructionFromInorderPostOrder
         static void Main(string[] args)
         {
             //int[] inorder = new int[] { 9, 3, 15, 20, 7 };
-           int[] inorder = new int[] { 4, 8, 2, 5, 1, 6, 3, 7 };
-           // int[] postorder = new int[] { 9, 15, 7, 20, 3 };
+            int[] inorder = new int[] { 4, 8, 2, 5, 1, 6, 3, 7 };
+            // int[] postorder = new int[] { 9, 15, 7, 20, 3 };
             int[] postorder = new int[] { 8, 4, 5, 2, 6, 7, 3, 1 };
             var res = TreeNode.BuildTree(inorder, postorder);
             Console.WriteLine(res.val);
@@ -31,30 +31,59 @@ namespace BTreeConstructionFromInorderPostOrder
 
         public static TreeNode BuildTree(int[] inorder, int[] postorder)
         {
-            if (inorder == null || postorder == null || inorder.Length == 0 || postorder.Length == 0) return null;
-            if (inorder.Length == 1) return new TreeNode(inorder[0]);
-            if (postorder.Length == 1) return new TreeNode(postorder[0]);
+            //     if (inorder == null || postorder == null || inorder.Length == 0 || postorder.Length == 0) return null;
+            //     if (inorder.Length == 1) return new TreeNode(inorder[0]);
+            //     if (postorder.Length == 1) return new TreeNode(postorder[0]);
 
-            List<int> inorderList = inorder.ToList();
-            List<int> postorderlist = postorder.ToList();
+            //     List<int> inorderList = inorder.ToList();
+            //     List<int> postorderlist = postorder.ToList();
 
-            int rootIndex = postorderlist.Count - 1;
-            TreeNode root = new TreeNode(postorderlist[rootIndex]);
+            //     int rootIndex = postorderlist.Count - 1;
+            //     TreeNode root = new TreeNode(postorderlist[rootIndex]);
 
-            int inorderrootIndex = inorderList.IndexOf(postorderlist[rootIndex]);
-            List<int> inorderSubList = inorderList.GetRange(0, inorderrootIndex);
-            List<int> postOrderSubList = postorderlist.GetRange(0, inorderrootIndex);
+            //     int inorderrootIndex = inorderList.IndexOf(postorderlist[rootIndex]);
+            //     List<int> inorderSubList = inorderList.GetRange(0, inorderrootIndex);
+            //     List<int> postOrderSubList = postorderlist.GetRange(0, inorderrootIndex);
 
-            root.left = BuildTree(inorderSubList.ToArray(), postOrderSubList.ToArray());
+            //     root.left = BuildTree(inorderSubList.ToArray(), postOrderSubList.ToArray());
 
-            inorderSubList = inorderList.GetRange(inorderrootIndex+1, inorderList.Count- (inorderrootIndex + 1));
-            postOrderSubList = postorderlist.GetRange(inorderrootIndex, postorderlist.Count - inorderrootIndex-1);
+            //     inorderSubList = inorderList.GetRange(inorderrootIndex+1, inorderList.Count- (inorderrootIndex + 1));
+            //     postOrderSubList = postorderlist.GetRange(inorderrootIndex, postorderlist.Count - inorderrootIndex-1);
 
-            root.right = BuildTree(inorderSubList.ToArray(), postOrderSubList.ToArray());
+            //     root.right = BuildTree(inorderSubList.ToArray(), postOrderSubList.ToArray());
+            //     return root;
+            return Helper(inorder, postorder, 0, inorder.Length - 1, postorder.Length - 1);
+
+        }
+
+        /// <summary>
+        /// Better Implementation as per LeetCode
+        /// </summary>
+        /// <param name="inorder"></param>
+        /// <param name="postorder"></param>
+        /// <param name="idxInstart"></param>
+        /// <param name="idxInEnd"></param>
+        /// <param name="idxPost"></param>
+        /// <returns></returns>
+        private static TreeNode Helper(int[] inorder, int[] postorder, int idxInstart, int idxInEnd, int idxPost)
+        {
+            if (idxPost < 0 || idxInstart > idxInEnd)
+                return null;
+
+            TreeNode root = new TreeNode(postorder[idxPost]); ;
+
+            int i = idxInstart;
+
+            for (; i < idxInEnd; i++)
+                if (inorder[i] == root.val)
+                    break;
+
+            root.left = Helper(inorder, postorder, idxInstart, i - 1, idxPost - 1 - (idxInEnd - i)); ;
+            root.right = Helper(inorder, postorder, i + 1, idxInEnd, idxPost - 1);
+
             return root;
-
         }
     }
 
-    
+
 }
