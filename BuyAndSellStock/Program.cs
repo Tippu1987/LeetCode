c77b478566179bd22f5071560746ebc1490ba5a7
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-08-03 05:47:48 -0700

    Working

---
+++ BuyAndSellStock/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BuyAndSellStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = new int[] { 2, 4, 1 };
+            Console.WriteLine(MaxProfit(prices));
+        }
+
+        /// <summary>
+        /// Pavan Didn't come thru this soln. This is a DP. Need to get better at Identifying DP Pattern
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns></returns>
+        public static int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 0) return 0;
+            int profit = 0, minPrice = int.MaxValue;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                    minPrice = prices[i];
+                else if (prices[i] - minPrice > profit)
+                    profit = prices[i] - minPrice;
+            }
+            return profit;
+        }
+    }
+}
