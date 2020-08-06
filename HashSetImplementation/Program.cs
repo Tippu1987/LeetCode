989db8c4d9e35ddf35b5dcd43c9da67d7db2773b
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-08-05 22:17:33 -0700

    Working Code for Matching braces and Hashset Implementation

---
+++ HashSetImplementation/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HashSetImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //          ["MyHashSet","add","add","contains","contains","add","contains","remove","contains"]
+            //[[],[1],[2],[1],[3],[2],[2],[2],[2]]
+            var myhash = new MyHashSet();
+            myhash.Contains(72);
+            myhash.Remove(91);
+            myhash.Add(48);
+            myhash.Add(41);
+            myhash.Contains(96);
+            myhash.Remove(87);
+            myhash.Contains(48);
+        }
+    }
+
+    public class MyHashSet
+    {
+        private List<int>[] Buckets;
+        private int bucketSize = 7919;
+
+        /** Initialize your data structure here. */
+        public MyHashSet()
+        {
+            Buckets = new List<int>[bucketSize];
+
+            for (int i = 0; i < bucketSize; i++)
+            {
+                Buckets[i] = new List<int>();
+            }
+        }
+
+        public void Add(int key)
+        {
+            int index = getHash(key);
+            if (!Contains(key))
+                Buckets[index].Add(key);
+        }
+
+        public void Remove(int key)
+        {
+            int index = getHash(key);
+            Buckets[index].Remove(key);
+
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+            int index = getHash(key);
+            return Buckets[index].Contains(key);
+        }
+
+        private int getHash(int val)
+        {
+            if (val >= 0)
+            {
+                return val % bucketSize;
+            }
+            return -1;
+        }
+    }
+}
---
+++ MatchingBraces/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
+
+namespace MatchingBraces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("(]"));
+        }
+
+        public static bool IsValid(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return true;
+            Dictionary<char, char> charMap = new Dictionary<char, char>();
+            charMap.Add('(', ')');
+            charMap.Add('{', '}');
+            charMap.Add('[', ']');
+            Stack<char> st = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char curChar = s[i];
+                if (curChar == '(' || curChar == '{' || curChar == '[')
+                    st.Push(curChar);
+                else
+                {
+                    char stTop = st.Count > 0 ? st.Pop() : '#';
+                    if (charMap.ContainsKey(stTop))
+                        if (s[i] == charMap[stTop]) continue;
+                        else
+                            return false;
+                    return false;
+                }
+            }
+            return st.Count == 0;
+
+        }
+    }
+}
