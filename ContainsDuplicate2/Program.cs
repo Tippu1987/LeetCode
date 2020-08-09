490541fe9f86ecb3d60534e3f892acc09710708b
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-08-09 13:56:08 -0700

    Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.

---
+++ ContainsDuplicate2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ContainsDuplicate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 1, 2, 3 };
+            int k = 2;
+            Console.WriteLine(ContainsNearbyDuplicate(nums, k));
+        }
+
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0) return false;
+            Dictionary<int, List<int>> indices = new Dictionary<int, List<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (indices.ContainsKey(nums[i]))
+                {
+                    if (indices[nums[i]].Any(x => Math.Abs(x - i) <= k))
+                        return true;
+                    else
+                        indices[nums[i]].Add(i);
+                }
+                else
+                    indices.Add(nums[i], new List<int> { i });
+            }
+            return false;
+        }
+    }
+}
