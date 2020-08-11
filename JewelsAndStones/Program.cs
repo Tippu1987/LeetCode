b5afd5ae6fab4166fa9585b9e29832722bdc7f08
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-08-11 05:26:57 -0700

    You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
    The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".

---
+++ JewelsAndStones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelsAndStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumJewelsInStones("z", "ZZ"));
+        }
+
+        public static int NumJewelsInStones(string J, string S)
+        {
+            if (string.IsNullOrEmpty(J) || string.IsNullOrEmpty(S)) return 0;
+
+            int count = 0; HashSet<char> jewels = new HashSet<char>(J);
+            for (int i = 0; i < S.Length; i++)
+                if (jewels.Contains(S[i])) count++;
+            return count;
+        }
+    }
+}
