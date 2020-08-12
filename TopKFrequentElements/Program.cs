85d57dcb484e1c54155f0e9ec00b87a6f309e779
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-08-11 22:19:31 -0700

    Given a non-empty array of integers, return the k most frequent elements.
    Note:
    
    You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
    Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
    It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
    You can return the answer in any order.

---
+++ TopKFrequentElements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopKFrequentElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
+            Console.WriteLine(TopKFrequent(nums,2));
+        }
+
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0 || k <= 0) return null;
+            Dictionary<int, int> numCount = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numCount.ContainsKey(nums[i]))
+                    numCount[nums[i]] += 1;
+                else
+                    numCount[nums[i]] = 1;
+            }
+            var topKelements = numCount.OrderByDescending(x => x.Value).Take(k);
+            return topKelements.Select(x => x.Key).ToArray();
+        }
+    }
+}
