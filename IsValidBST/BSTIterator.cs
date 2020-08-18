c99cca6c5243c62854d1181695e0056640a6354b
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-08-18 05:39:44 -0700

    Using Stack and recursion

--- IsValidBST/BSTIterator.cs
+++ IsValidBST/BSTIterator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace IsValidBST
@@ -10,6 +11,7 @@ namespace IsValidBST
         TreeNode root = null;
         int[] nodes = null;
         int curIndex;
+        Stack<TreeNode> tnodes = new Stack<TreeNode>();
 
         public BSTIterator(TreeNode root)
         {
@@ -18,25 +20,20 @@ namespace IsValidBST
             curIndex = 0;
         }
 
-        /* public int[] FlattenBST(TreeNode root){
-             if(root==null) return new int[1];
-             Stack<TreeNode> tnodes=new Stack<TreeNode>();
-             tnodes.Push(root);
-             while(tnodes.Count>0 || root!=null){
-                 root=tnodes.Pop();
-                 while(root!=null){
-                     if(root.left!=null){
-                         tnodes.Push(root);
-                         root=root.left;
-                     }
-                     else{
-                         nodes[nodes.Length++]=root.val;
-                         root=root.right;
-                     }
-                 }
+        public void FlattenBST(TreeNode root)
+        {
+            if (root == null) return;
+            InsertLeftNodes(root);
+        }
 
-             }
-         }*/
+        public void InsertLeftNodes(TreeNode r)
+        {
+            while (r != null)
+            {
+                tnodes.Push(r);
+                r = r.left;
+            }
+        }
 
         public static int[] GetInorderList(TreeNode node, List<int> res)
         {
@@ -60,11 +57,29 @@ namespace IsValidBST
                 return nodes[curIndex++];
         }
 
+        public int NextUsingStack()
+        {
+            TreeNode curnode = null;
+            //Give your Inorder Successor
+            if (tnodes.Any())
+            {
+                curnode = tnodes.Pop();
+                InsertLeftNodes(curnode.right);
+            }
+            return curnode == null ? int.MinValue : curnode.val;
+        }
+
         /** @return whether we have a next smallest number */
         public bool HasNext()
         {
             //Verifies if you have an Inorder Successor
             return curIndex < nodes?.Length;
+        }
+
+        public bool HasNextUsingStack()
+        {
+            //Verifies if you have an Inorder Successor
+            return tnodes.Any();
         }
     }
 }
