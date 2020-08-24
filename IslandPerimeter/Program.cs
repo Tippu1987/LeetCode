7b3d7c1f94d662855b563a48e9df3baa9da56dbe
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-08-23 20:02:03 -0700

    You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
    Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
    
    The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island

---
+++ IslandPerimeter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IslandPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Solution
+    {
+        public int IslandPerimeter(int[][] grid)
+        {
+            if (grid == null || grid.Length == 0) return 0;
+            int result = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        result += 4;
+                        if (i > 0 && grid[i - 1][j] == 1)
+                            result -= 2;
+                        if (j > 0 && grid[i][j - 1] == 1)
+                            result -= 2;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
