903a53dca2a98c4609ac68c3e1b7d8efe0e9691c
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-10-26 21:45:55 -0700

    Merge Intervals & WordSearch Working

---
+++ MergeIntervals/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeIntervals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[][] inp = new int[][]
+            {
+               new int[]{0, 0 },
+               new int[]{1, 2 },
+               new int[]{5, 5 },
+               new int[]{2, 4 },
+               new int[]{3, 3 },
+               new int[]{5, 6 },
+               new int[]{5, 6 },
+               new int[]{4, 6 },
+               new int[]{0, 0 },
+               new int[]{1, 2 },
+               new int[]{0, 2 },
+               new int[]{4, 5 }
+            };
+            Console.WriteLine(s.Merge(inp));
+        }
+    }
+    public class Solution
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            if (intervals == null || intervals.Length == 0) return new int[][] { };
+            var resHS = new LinkedList<int[]>();
+            Array.Sort(intervals, (x, y) => x[0].CompareTo(y[0]));
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                if (resHS.Count == 0 || resHS.Last.Value[1] < intervals[i][0])
+                    resHS.AddLast(intervals[i]);
+                else
+                    resHS.Last.Value[1] = Math.Max(resHS.Last.Value[1], intervals[i][1]);
+            }
+            return resHS.ToArray();
+        }
+    }
+}
---
+++ WordSearch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace WordSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            char[][] board = new[]
+            {
+                new char[] { 'A', 'B', 'C', 'E' },
+                new char[] { 'S', 'F', 'C', 'S' },
+                new char[] { 'A', 'D', 'E', 'E' }
+            };
+
+            Console.WriteLine(s.Exist(board, "ABCB"));
+        }
+    }
+
+    public class Solution
+    {
+        public bool Exist(char[][] board, string word)
+        {
+            if (board == null || board.Length == 0 || string.IsNullOrEmpty(word)) return false;
+            bool wordExists = false;
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[0].Length; j++)
+                {
+                    if (board[i][j] == word[0] && !wordExists)
+                    {
+                        wordExists = DFS(board, i, j, word);
+                    }
+                }
+            }
+            return wordExists;
+        }
+        private bool DFS(char[][] board, int i, int j, string word)
+        {
+            if (string.IsNullOrEmpty(word)) return true;
+            if (board == null || i < 0 || i >= board.Length || j < 0 || j >= board[0].Length || board[i][j] != word[0]) return false;
+            char temp = board[i][j];
+            board[i][j] = ' ';
+            var found = DFS(board, i, j + 1, word.Substring(1)) ||
+             DFS(board, i, j - 1, word.Substring(1)) ||
+             DFS(board, i + 1, j, word.Substring(1)) ||
+             DFS(board, i - 1, j, word.Substring(1));
+            board[i][j] = temp;
+            return found;
+        }
+    }
+}
