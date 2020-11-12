379d16bd39498ac2cc83ec7329735bf6819d831a
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-11-11 23:22:31 -0700

    Working

---
+++ FormatLicenseKey/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FormatLicenseKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().LicenseKeyFormatting("2-5g-3-J", 2)); ;
+        }
+    }
+
+    public class Solution
+    {
+        public string LicenseKeyFormatting(string S, int K)
+        {
+            if (string.IsNullOrEmpty(S)) return S;
+            S = S.Replace("-", "").ToUpper();
+
+            int cnt = 0;
+            string r = "";
+            for (int i = S.Length - 1; i >= 0; i--)
+            {
+                if (cnt != K)
+                {
+                    r = S[i] + r;
+                    cnt++;
+                }
+                else
+                {
+                    r = S[i] + "-" + r;
+                    cnt = 1;
+                }
+            }
+            return r;
+        }
+    }
+}
---
+++ StringCompression/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace StringCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' };
+            Console.WriteLine(new Solution().Compress(chars));
+        }
+    }
+    public class Solution
+    {
+        public int Compress(char[] chars)
+        {
+            if (chars == null || chars.Length == 0) return 0;
+            int cnt = 1, oldindex = 0;
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i+1 < chars.Length; i++)
+            {
+                if (chars[i] == chars[i + 1])
+                    cnt++;
+                else
+                {
+                    s.Append(cnt > 1 ? chars[oldindex].ToString() + cnt : chars[oldindex].ToString());
+                    oldindex++;
+                    if (cnt > 1) chars[oldindex] = (i - oldindex+1).ToString()[0];
+                    cnt = 1;
+                }
+            }
+            s.Append(cnt > 1 ? chars[oldindex].ToString() + cnt : chars[oldindex].ToString());
+            return s.Length;
+        }
+    }
+}
